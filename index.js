b85744e5589a122ad8a1011213a0190cbd5aa4d7
Julia Baur <juliabaur1@gmx.de>
2025-03-11 11:54:58 +0100

    added userRouter to index.js in root directory

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const sequelize = require('./db');
+const dotenv = require('dotenv');
+dotenv.config(); // Loading .env file
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Loading user router
+const userRouter = require('./routers/userRouter');
+app.use('/api/users', userRouter);
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// db connection and server start
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`);
+    });
+}).catch((err) => {
+    console.error('Database connection failed:', err);
+});
